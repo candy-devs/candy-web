8d7fddce4bc1389f39f95db3f6da09fb52687189
rollrat <rollrat.cse@gmail.com>
2022-04-20 11:16:58 +0900

    rank-list: fix bug that rank list is not shown when display none

--- src/components/RankList.tsx
+++ src/components/RankList.tsx
@@ -1,11 +1,17 @@
-import React, { LegacyRef, SyntheticEvent, useCallback, useEffect, useRef } from "react";
+import React, {
+  LegacyRef,
+  SyntheticEvent,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
 import { useState } from "react";
 import "./RankList.scss";
 
 type RankListItemProps = {
-  rank: string,
-  name: string,
-  variance: number,
+  rank: string;
+  name: string;
+  variance: number;
 };
 
 function RankListItem({ rank, name, variance }: RankListItemProps) {
@@ -20,8 +26,9 @@ function RankListItem({ rank, name, variance }: RankListItemProps) {
           {Math.abs(variance)}
           {variance === 0 ? null : (
             <div
-              className={`rank-list-variance-triangle rank-list-variance-triangle-${variance > 0 ? "up" : "down"
-                }`}
+              className={`rank-list-variance-triangle rank-list-variance-triangle-${
+                variance > 0 ? "up" : "down"
+              }`}
             />
           )}
         </div>
@@ -34,15 +41,16 @@ function RankListPage() {
   // const ref = useRef() as React.MutableRefObject<HTMLDivElement>;
   // ref.current.style.setProperty("--rank-list-page-width", `100px`);
 
-  return <div className="rank-list-page" style={{padding: "0"}}>
-    <RankListItem rank="1" name="정치 사회" variance={20} />
-    <RankListItem rank="2" name="코인" variance={30} />
-    <RankListItem rank="3" name="주식" variance={6} />
-    <RankListItem rank="4" name="여행" variance={-12} />
-    <RankListItem rank="5" name="축구" variance={-12} />
-    {/* <RankListItem rank="6" name="비지니스 경제" variance={-12} /> */}
-
-  </div>
+  return (
+    <div className="rank-list-page" style={{ padding: "0" }}>
+      <RankListItem rank="1" name="정치 사회" variance={20} />
+      <RankListItem rank="2" name="코인" variance={30} />
+      <RankListItem rank="3" name="주식" variance={6} />
+      <RankListItem rank="4" name="여행" variance={-12} />
+      <RankListItem rank="5" name="축구" variance={-12} />
+      {/* <RankListItem rank="6" name="비지니스 경제" variance={-12} /> */}
+    </div>
+  );
 }
 
 export default function RankList() {
@@ -57,11 +65,16 @@ export default function RankList() {
   var onScrollLeft = 0;
 
   useEffect(() => {
-    const { style } = ref.current;
-    setWidth(ref.current.getBoundingClientRect().width);
-    style.setProperty("--rank-list-page-width", `${ref.current.getBoundingClientRect().width}px`);
-    scrollRef.current.scrollLeft = Math.round(onScrollLeft / width) * width;
-    setPageIndex(Math.round(onScrollLeft / width));
+    if (ref.current.getBoundingClientRect().width !== 0) {
+      const { style } = ref.current;
+      setWidth(ref.current.getBoundingClientRect().width);
+      style.setProperty(
+        "--rank-list-page-width",
+        `${ref.current.getBoundingClientRect().width}px`
+      );
+      scrollRef.current.scrollLeft = Math.round(onScrollLeft / width) * width;
+      setPageIndex(Math.round(onScrollLeft / width));
+    }
   }, [ref, scrollRef, onScrollLeft, width]);
 
   const resize = () => {
@@ -71,16 +84,15 @@ export default function RankList() {
   };
 
   useEffect(() => {
-    window.addEventListener('resize', resize);
+    window.addEventListener("resize", resize);
     return () => {
-      window.removeEventListener('resize', resize);
-    }
+      window.removeEventListener("resize", resize);
+    };
   }, [ref]);
 
   const onScroll = function (event: SyntheticEvent<HTMLDivElement>) {
     onScrolling = true;
-    if (onScrollTimer !== -1)
-      clearTimeout(onScrollTimer);
+    if (onScrollTimer !== -1) clearTimeout(onScrollTimer);
     onScrollTimer = window.setTimeout(onScrollEnd, 100);
     onScrollLeft = event.currentTarget.scrollLeft;
   };
@@ -91,24 +103,28 @@ export default function RankList() {
       scrollRef.current.scrollLeft = Math.round(onScrollLeft / width) * width;
       setPageIndex(Math.round(onScrollLeft / width));
     }
-  }
+  };
 
   const onMouseDown = () => {
     onMouse = true;
-    if (onScrollTimer !== -1)
-      clearTimeout(onScrollTimer);
-  }
+    if (onScrollTimer !== -1) clearTimeout(onScrollTimer);
+  };
 
   const onMouseUp = () => {
     onMouse = false;
-    if (onScrollTimer !== -1)
-      clearTimeout(onScrollTimer);
+    if (onScrollTimer !== -1) clearTimeout(onScrollTimer);
     onScrollTimer = window.setTimeout(onScrollEnd, 100);
-  }
+  };
 
   return (
     <div ref={ref} className="rank-list-wrapper">
-      <div ref={scrollRef} className="rank-list" onScroll={onScroll} onMouseDown={onMouseDown} onMouseUp={onMouseUp}>
+      <div
+        ref={scrollRef}
+        className="rank-list"
+        onScroll={onScroll}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+      >
         <RankListPage />
         <RankListPage />
         <RankListPage />
@@ -116,7 +132,11 @@ export default function RankList() {
       </div>
       <div className="rank-list-dots">
         {[...Array(4).keys()].map((_, index) => (
-          <div className={`rank-list-dot ${index === pageIndex ? 'selected' : 'unselected'}`}></div>
+          <div
+            className={`rank-list-dot ${
+              index === pageIndex ? "selected" : "unselected"
+            }`}
+          ></div>
         ))}
       </div>
     </div>
