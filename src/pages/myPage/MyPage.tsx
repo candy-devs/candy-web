6f280de8761794d9b5886ee06daa5e8605862f68
rollrat <rollrat.cse@gmail.com>
2022-04-20 10:56:42 +0900

    mypage: expand down button width size

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -44,13 +44,13 @@ export default function MyPage() {
               },
             ]}
           />
-          <div className="asdf">
-
-          </div>
-          <div className="my-setting-panel-down" onClick={panelDownClick}>
+          <div className="my-setting-panel-down" onClick={panelDownClick} style={{padding: "0 0 0 16px"}}>
             <DownIcon />
           </div>
         </div>
+          <div className="asdf">
+
+          </div>
         <div style={{ height: "40px" }}>
           <HorizontalScroll>
             {["박근혜", "드라큘라", "정치 · 사회", ...Array(8).keys()].map(
