8cd65ef7e307f70609e97edb3866116c8eedf13c
rollrat <rollrat.cse@gmail.com>
2022-04-23 19:57:33 +0900

    index: remove request session

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { requestSession } from './api/Session';
-import { getCookie } from './utils/Cookie';
-
-function init() {
-  if (getCookie("connect.sid") === null) {
-    requestSession();
-  }
-}
-
-init();
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { requestSession } from "./api/Session";
+import { getCookie } from "./utils/Cookie";
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
