c9e0c15ba3572a45dbf5effa967eb60604aad3b7
rollrat <rollrat.cse@gmail.com>
2022-04-23 20:21:44 +0900

    package: add redux package

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.scss";
+import { Provider } from "react-redux";
 import { Navigate, Route, Routes } from "react-router";
 import SearchPage from "./pages/search/SearchPage";
 import HomePage from "./pages/home/HomePage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import BookmarkPage from "./pages/bookmark/BookmarkPage";
 import WritePage from "./pages/write/WritePage";
+import store from "./store";
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<HomePage />} />
@@ -17,7 +19,7 @@ function App() {
         <Route path="/bookmark" element={<BookmarkPage />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
-    </div>
+    </Provider>
   );
 }
 
