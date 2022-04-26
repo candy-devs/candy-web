d1b4a40ee160c0bd244e1fa4b8a66a8bc5c3f304
rollrat <rollrat.cse@gmail.com>
2022-04-26 10:01:47 +0900

    page: add title, search bar to SelectArticleTagPage

--- src/pages/selectArticleTag/SelectArticleTagPage.tsx
+++ src/pages/selectArticleTag/SelectArticleTagPage.tsx
@@ -1,9 +1,45 @@
-import './SelectArticleTagPage.scss';
+import styles from "./SelectArticleTagPage.module.scss";
 
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { CloseIcon, SearchIcon } from "../../assets/Icons";
+import IosSegmentedControl from "../../components/IosSegmentedControl";
 
 export default function SelectArticleTagPage() {
+  const [searchType, setSearchType] = useState(0);
+
   return (
-    <div className="select-article-tag" style={{zIndex: 30}}>SelectArticleTagPage</div>
-  )
+    <div className={styles.SelectArticleTag} style={{ zIndex: 30 }}>
+      <div className={styles.Body}>
+        <div className={styles.TitleBar}>
+          <span>게시판 · 태그 선택</span>
+          <CloseIcon />
+        </div>
+        <div className={styles.SearchArea}>
+          <IosSegmentedControl
+            name="selectArticleTag"
+            callback={(val: number, index: number) => setSearchType(val)}
+            controlRef={useRef()}
+            defaultIndex={0}
+            minWitdh={50}
+            segments={[
+              {
+                label: "게시판",
+                value: 0,
+                ref: useRef(),
+              },
+              {
+                label: "태그",
+                value: 1,
+                ref: useRef(),
+              },
+            ]}
+          />
+          <div className={styles.SearchBar}>
+            <SearchIcon />
+            <input />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
