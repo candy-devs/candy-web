6e7a0489ff199a20f6c5e00d84ff909c1198e7db
rollrat <rollrat.cse@gmail.com>
2022-04-26 10:35:12 +0900

    page: layout selectArticleTag page

--- src/pages/selectArticleTag/SelectArticleTagPage.tsx
+++ src/pages/selectArticleTag/SelectArticleTagPage.tsx
@@ -5,6 +5,39 @@ import { CloseIcon, SearchIcon } from "../../assets/Icons";
 import IosSegmentedControl from "../../components/IosSegmentedControl";
 import { DialogChildParameter } from "../../controller/dialog/DialogBase";
 
+interface TagSelectItemType {
+  name: string;
+}
+
+function TagSelectItem({ name }: TagSelectItemType) {
+  return (
+    <div className={styles.TagSelectItem}>
+      <div className={styles.TagSelectItemBody}>
+        <div className={styles.TagSelectItemName}>{name}</div>
+        <div className={styles.TagSelectItemCheck}></div>
+      </div>
+      <div className={styles.TagSelectItemDivider} />
+    </div>
+  );
+}
+
+interface TagSelectGroupType {
+  title: string;
+}
+
+function TagSelectGroup({ title }: TagSelectGroupType) {
+  return (
+    <div className={styles.TagSelectGroup}>
+      <div className={styles.TagSelectTitle}>{title}</div>
+      <div className={styles.TagSelectItemsArea}>
+        <TagSelectItem name="정치 · 사회" />
+        <TagSelectItem name="정치 · 사회" />
+        <TagSelectItem name="정치 · 사회" />
+      </div>
+    </div>
+  );
+}
+
 export default function SelectArticleTagPage({
   onClose,
 }: DialogChildParameter) {
@@ -43,6 +76,12 @@ export default function SelectArticleTagPage({
             <SearchIcon />
             <input />
           </div>
+        </div>
+        <div className={styles.TagSelectArea}>
+          <TagSelectGroup title="엔터테인먼트 · 예술" />
+          <TagSelectGroup title="생활 · 노하우 · 쇼핑" />
+          <TagSelectGroup title="취미 · 여가 · 여행" />
+          <TagSelectGroup title="지식 · 동향" />
         </div>
       </div>
     </div>
